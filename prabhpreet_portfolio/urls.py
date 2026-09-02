58cd0b9abc049f2f18ec76db72c5e949ddf27411
prabhpreet2026 <shashisaroj2025@gmail.com>
2026-09-02 13:04:45 +0530

    Restore URL configuration and views in urls.py

--- prabhpreet_portfolio/urls.py
+++ prabhpreet_portfolio/urls.py
@@ -59,10 +59,4 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('', views.home, name='home'),
 ]
-
-if settings.DEBUG:
-
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
