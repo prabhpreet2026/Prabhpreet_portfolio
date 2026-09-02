b0237cdaf44029e84080aea71ee6e2260c6ab59e
prabhpreet2026 <shashisaroj2025@gmail.com>
2026-09-02 13:03:00 +0530

    Update settings.py

--- prabhpreet_portfolio/settings.py
+++ prabhpreet_portfolio/settings.py
@@ -108,8 +108,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [y]
 
 
-MEDIA_URL = "/"
+#MEDIA_URL = "/"
+#MEDIA_ROOT = BASE_DIR / "media"
 
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 
